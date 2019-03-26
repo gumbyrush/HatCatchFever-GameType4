d80b22c9f49d8b9f7ddf44d48a47ffb68631b6b5
gumbyrush <matthew-rush@uiowa.edu>
2019-03-26 09:40:23 -0500

    HatCatchFever game 4
    HatCatchFever game 4

---
+++ Assets/Hat Trick - The Catch Game/Scripts/HT_Score.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HT_Score : MonoBehaviour {
+
+	public GUIText scoreText;
+	public int ballValue;
+
+	private int score;
+
+	void Start () {
+		score = 0;
+		UpdateScore ();
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		score += ballValue * Random.Range(0,2) * 2-1 ;
+		UpdateScore ();
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		if (collision.gameObject.tag == "Bomb") {
+			score += ballValue * Random.Range(0, 2) * 2 - 1;
+			UpdateScore ();
+		}
+	}
+
+	void UpdateScore () {
+		scoreText.text = "SCORE:\n" + score;
+	}
+}
